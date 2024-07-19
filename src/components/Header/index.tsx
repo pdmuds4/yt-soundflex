4442963b93fedbd2409ccba73111cefdd8cedb5a
pdmuds4 <ryosuke3191@gmail.com>
2024-07-19 17:24:56 +0900

    headerを実装しました

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,32 @@
+'use client';
+import { ChakraProvider, Box, Button, Heading, Img } from '@chakra-ui/react';
+import { s__headerBody, s__headerBtn, s__headerIcon } from './style';
+import icon from "public/icon.svg";
+
+const Header: React.FC = () => {
+    return (
+        <Box sx={s__headerBody}>
+            <Button 
+                sx={s__headerBtn} 
+                variant='ghost'
+                colorScheme='blackAlpha'
+                // onClick={}
+            >
+                <Img sx={s__headerIcon} src={icon} />
+                <Heading fontSize={{base: 'x-large', sm: 'xx-large'}}>YoutubeSoundFlex</Heading>
+            </Button>
+        </Box>
+    );
+}
+
+//export default Header;
+
+const DebugHeader: React.FC = () => {
+    return (
+        <ChakraProvider>
+            <Header />
+        </ChakraProvider>
+    );
+}
+
+export default DebugHeader;
---
+++ src/components/Header/style.tsx
@@ -0,0 +1,24 @@
+import { SystemStyleObject } from "@chakra-ui/react";
+
+export const s__headerBody: SystemStyleObject = {
+    position: 'absolute',
+    width: '100vw',
+    display: 'flex',
+    top: 0,
+    left: 0,
+    backgroundColor: '#ff0026',
+    boxShadow: '0px 5px 5px 0px rgb(116, 116, 116)',
+}
+
+export const s__headerBtn: SystemStyleObject = {
+    margin: 'auto',
+    height: '10vh',
+    width: '100vw',
+    borderRadius: 0,
+    color: 'white',
+}
+
+export const s__headerIcon: SystemStyleObject = {
+    width: 'auto',
+    height: '10vh',
+}
