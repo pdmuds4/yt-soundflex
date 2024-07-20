eeaf80fab4319d19601dea0479d14b43d2be8337
pdmuds4 <ryosuke3191@gmail.com>
2024-07-21 00:19:55 +0900

    MoviesTableを実装しました

---
+++ src/components/MoviesTable/index.tsx
@@ -0,0 +1,33 @@
+import { ChakraProvider, TableContainer, Table, Thead, Tbody, Tr, Th } from "@chakra-ui/react";
+
+const MoviesTable: React.FC = () => {
+    return (
+        <TableContainer width={{base: '100%', md: '50%'}}>
+            <Table size='sm' variant='striped'>
+                <Thead>
+                    <Tr>
+                        <Th></Th>
+                        <Th>サムネイル</Th>
+                        <Th>保存ファイル名</Th>
+                        <Th>保存フォーマット</Th>
+                    </Tr>
+                </Thead>
+                <Tbody>
+                    { /* <MovieTableData /> */ }
+                </Tbody>
+            </Table>
+        </TableContainer>
+    );
+}
+
+// export default MoviesTable;
+
+const DebugMoviesTable: React.FC = () => {
+    return (
+        <ChakraProvider>
+            <MoviesTable />
+        </ChakraProvider>
+    );
+}
+
+export default MoviesTable;
