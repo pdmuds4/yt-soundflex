1bc6ab60bbd04c9c554a1afdee59187e76cfa460
pdmuds4 <ryosuke3191@gmail.com>
2024-07-23 19:10:42 +0900

    list操作のボタンを実装

--- src/components/MoviesList/index.tsx
+++ src/components/MoviesList/index.tsx
@@ -1,11 +1,35 @@
-import { Box } from "@chakra-ui/react";
+import { Grid, GridItem, Button } from "@chakra-ui/react";
+import { DownloadIcon, RepeatIcon } from "@chakra-ui/icons";
 import MoviesTable from "./MoviesTable";
 
 const MoviesList: React.FC = () => {
     return (
-        <Box>
-            <MoviesTable />
-        </Box>
+        <Grid templateColumns='repeat(2, 1fr)' gap={4}>
+            <GridItem colSpan={1}>
+                <Button 
+                    w='100%' 
+                    size='md'
+                    colorScheme="green"
+                    rightIcon={<DownloadIcon />}
+                    // onClick = {}
+                    // isDisabled={}
+                >一括ダウンロード</Button>
+            </GridItem>
+            <GridItem colSpan={1}>
+                <Button 
+                    w='100%' 
+                    size='md'
+                    colorScheme="blue"
+                    variant='outline'
+                    rightIcon={<RepeatIcon />}
+                    // onClick = {}
+                    // isDisabled={}
+                >リセット</Button>
+            </GridItem>
+            <GridItem colSpan={2}>
+                <MoviesTable />
+            </GridItem>
+        </Grid>
     )
 }
 
